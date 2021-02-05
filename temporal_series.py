beed30093932a12445f67c31116d26214e6ce780
Ludmilla Manera Conti <ludmillaconti@gmail.com>
2021-02-05 11:19:54 -0300

    Add files via upload

---
+++ temporal_series.py
@@ -0,0 +1,22 @@
+from numba import cuda, jit
+
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+#mudar dinamicamente o default da biblioteca matplotlib.rcParams
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+zip_path = tf.keras.utils.get_file(origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
---
+++ usercollection.py
@@ -0,0 +1,30 @@
+"""
+1 - Funções noemadas (def)
+2 - Funções anonimas (lambda)
+3 - Funções de classe (class)
+"""
+
+def nomeada_soma(x + y):
+    return x + y
+
+ anonima_soma = lambda x,y:  x + y #não tem return na lambda
+
+ class classe_soma:
+     def __init__(self, x, y):
+         self.x = x #vai receber x
+         self.y = y # vai receber y
+
+     def __call__(self):
+         return self.x + self.y
+
+
+  """
+Funções nomeadas (padrões)
+  """
+
+  def soma(x,y):
+      return x + y
+  print(soma(2,2)) #tudo no python é objeto
+  print(soma.__doc__) #explica o que esta dentro da função e alocação de memoria
+  print(help(soma)) #tbm explica se estiver escrito dentro da função """
+  
